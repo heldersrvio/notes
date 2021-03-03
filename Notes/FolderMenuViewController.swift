e39a19dd8244ef60546942e2dc569a7a9eb6b2d8
Helder Sérvio <heldersrvio@gmail.com>
2021-03-03 18:01:15 -0300

    Customized folder menu navigation bar

--- Notes/FolderMenuViewController.swift
+++ Notes/FolderMenuViewController.swift
@@ -18,7 +18,23 @@ class FolderMenuViewController: UITableViewController, UINavigationBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = folderName
+        let button = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.close, target: self, action: #selector(close))
+        navigationItem.rightBarButtonItem = button
+        let navigationView = UIView()
+        let label = UILabel()
+        label.text = folderName
+        label.font = UIFont.boldSystemFont(ofSize: 16.0)
+        label.sizeToFit()
+        label.center = CGPoint(x: navigationView.center.x - label.frame.size.height * 3, y: navigationView.center.y)
+        let folderIcon = UIImageView()
+        folderIcon.image = UIImage(systemName: "folder.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .regular, scale: .large))?.withTintColor(UIColor(red: 0.88, green: 0.67, blue: 0, alpha: 1), renderingMode: .alwaysOriginal)
+        let imageAspect = folderIcon.image!.size.width / folderIcon.image!.size.height
+        folderIcon.frame = CGRect(x: label.frame.origin.x - label.frame.size.height * imageAspect * 1.7, y: label.frame.origin.y - label.frame.size.height * 0.25, width: label.frame.size.height * 1.5 * imageAspect, height: label.frame.size.height * 1.5)
+        folderIcon.contentMode = UIView.ContentMode.scaleAspectFit
+        navigationView.addSubview(label)
+        navigationView.addSubview(folderIcon)
+        navigationItem.titleView = navigationView
+        navigationView.sizeToFit()
         tableView.tableFooterView = UIView()
     }
     
@@ -42,5 +58,9 @@ class FolderMenuViewController: UITableViewController, UINavigationBarDelegate {
         default:
             deleteFolder(index)
         }
+    }
+    
+    @objc func close() {
+        dismiss(animated: true)
     }
 }
